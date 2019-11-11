af1ab52b164009cbdc405d2c8f77dcac7c77afad
magonj1 <magonj1@PC-U13000731.int.unisa.ac.za>
2019-11-11 15:55:53 +0200

    port changes

--- src/main/java/magongwa/jeremia/student/model/Student.java
+++ src/main/java/magongwa/jeremia/student/model/Student.java
@@ -1,5 +1,6 @@
 package magongwa.jeremia.student.model;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,13 +8,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Student {
 	
 	@Id
-	private String student_id;
+	private ObjectId student_id;
 	private String student_No;
 	private String firstName;
 	private String lastName;
 	private int  age;
+	private ObjectId group_id;
 	
 	
+	public ObjectId getGroup_id() {
+		return group_id;
+	}
+
+	public void setGroup_id(ObjectId group_id) {
+		this.group_id = group_id;
+	}
+
 	public Student(String student_No,String firstName,String lastName,int age)
 	{
 		 this.student_No = student_No;
@@ -22,10 +32,10 @@ public class Student {
 		 this.age = age;
 	}
 	
-	public String getStudent_id() {
+	public ObjectId getStudent_id() {
 		return student_id;
 	}
-	public void setStudent_id(String student_id) {
+	public void setStudent_id(ObjectId student_id) {
 		this.student_id = student_id;
 	}
 	public String getStudent_No() {
